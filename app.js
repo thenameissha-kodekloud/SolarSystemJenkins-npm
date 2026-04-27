89c93ec7b1cafdbee67dcfb2b8dd2c76d6539093
unknown <kannanakash896@gmail.com>
2026-04-27 12:21:20 +0530

    unit-test-app11

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ app.use(cors())
 // ✅ Fix 1 - no callback, use .then/.catch
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    pass: process.env.MONGO_PASSWORD
 })
 .then(() => console.log("MongoDB Connection Successful"))
 .catch((err) => console.log("error!! " + err));
