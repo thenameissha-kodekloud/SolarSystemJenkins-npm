5bf834402a946010713fdb74b43d4e75aa35132c
unknown <kannanakash896@gmail.com>
2026-04-27 12:00:21 +0530

    unit-test-ap

--- app1.js
+++ app1.js
@@ -6,22 +6,28 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
+const serverless = require('serverless-http')
+
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-// ✅ Fix 1 - no callback, use .then/.catch
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
+}, function(err) {
+    if (err) {
+        console.log("error!! " + err)
+    } else {
+      //  console.log("MongoDB Connection Successful")
+    }
 })
-.then(() => console.log("MongoDB Connection Successful"))
-.catch((err) => console.log("error!! " + err));
 
 var Schema = mongoose.Schema;
+
 var dataSchema = new Schema({
     name: String,
     id: Number,
@@ -32,32 +38,38 @@ var dataSchema = new Schema({
 });
 var planetModel = mongoose.model('planets', dataSchema);
 
-// ✅ Fix 2 - async/await instead of callback
-app.post('/planet', async function(req, res) {
-    try {
-        const planetData = await planetModel.findOne({ id: req.body.id });
-        res.send(planetData);
-    } catch (err) {
-        res.status(500).send("Error in Planet Data");
-    }
+
+
+app.post('/planet',   function(req, res) {
+   // console.log("Received Planet ID " + req.body.id)
+    planetModel.findOne({
+        id: req.body.id
+    }, function(err, planetData) {
+        if (err) {
+            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
+            res.send("Error in Planet Data")
+        } else {
+            res.send(planetData);
+        }
+    })
 })
 
-app.get('/', async (req, res) => {
+app.get('/',   async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 app.get('/api-docs', (req, res) => {
     fs.readFile('oas.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            res.status(500).send('Error reading file');
-        } else {
-            res.json(JSON.parse(data));
-        }
+      if (err) {
+        console.error('Error reading file:', err);
+        res.status(500).send('Error reading file');
+      } else {
+        res.json(JSON.parse(data));
+      }
     });
-});
-
-app.get('/os', function(req, res) {
+  });
+  
+app.get('/os',   function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "os": OS.hostname(),
@@ -65,15 +77,21 @@ app.get('/os', function(req, res) {
     });
 })
 
-app.get('/live', function(req, res) {
+app.get('/live',   function(req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send({ "status": "live" });
+    res.send({
+        "status": "live"
+    });
 })
 
-app.get('/ready', function(req, res) {
+app.get('/ready',   function(req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send({ "status": "ready" });
+    res.send({
+        "status": "ready"
+    });
 })
 
-app.listen(3000, () => { console.log("Server successfully running on port - " + 3000); })
+app.listen(3000, () => { console.log("Server successfully running on port - " +3000); })
 module.exports = app;
+
+//module.exports.handler = serverless(app)
